bb643d16bc9686141275687d8e81dfe641ef4b3c
Spoofrs <alexwardcareer@gmail.com>
2024-01-16 06:14:35 -0700

    just small reformat

--- core/src/com/mygdx/game/util/Meteorite.java
+++ core/src/com/mygdx/game/util/Meteorite.java
@@ -14,10 +14,9 @@ public class Meteorite {
     Random random;
     public static HashMap<Meteorite, Rectangle> meteoriteHashMap = new HashMap<>();
     int x, y;
-    Integer[] spawnXLocation = { 80, 160, 240, 320, 400, 480, 560, 640 };
+    Integer[] spawnXLocation = {80, 160, 240, 320, 400, 480, 560, 640};
 
     public Meteorite() {
-
     }
 
     public Meteorite(int x) {
@@ -29,7 +28,7 @@ public class Meteorite {
         Texture meteoriteTexture = new Texture("meteorite.png");
         random = new Random();
         int meteoritePosition = random.nextInt(0, 8);
-        int randomRoll = random.nextInt(0, 500) + 1; //@TODO balance this so its not overwhelming
+        int randomRoll = random.nextInt(0, 500) + 1;
         if (randomRoll == 1 && meteoriteHashMap.isEmpty()) { //only allow a meteorite to be called if there isn't one on screen already
             meteoriteHashMap.put(new Meteorite(spawnXLocation[meteoritePosition]), new Rectangle(spawnXLocation[meteoritePosition], Gdx.graphics.getHeight(), 40, 60));
         }
@@ -38,16 +37,16 @@ public class Meteorite {
             batch.draw(meteoriteTexture, meteorite.x, meteorite.y, 40, 60);
             rectangleListener.set(meteorite.x, meteorite.y, 40, 40);
         }
+        //Intellij recommends this:
+        // meteoriteHashMap.values().removeIf(meteoriteListener -> meteoriteListener.y <= -100);
+        // reduces all code from line 44 to 48 with that, @TODO look into it and understand it (pretty sure its lambda?)
         Iterator<Rectangle> meteoriteIterator = meteoriteHashMap.values().iterator();
-        while(meteoriteIterator.hasNext()) {
+        while (meteoriteIterator.hasNext()) {
             Rectangle meteoriteListener = meteoriteIterator.next();
-            if(meteoriteListener.y <= -100) { // when meteorite is off-screen we remove it from the hashmap
+            if (meteoriteListener.y <= -100) { // when meteorite is off-screen we remove it from the hashmap
                 meteoriteIterator.remove();
             }
-
         }
-
-
     }
 
 
